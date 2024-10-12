51f13f492efc286c753003473125372ffe38665b
wal99d <i@waleed.digital>
2024-10-12 22:22:57 +0300

    added support for keymapping for git add and git commit

--- lua/c/plugins/fugitive.lua
+++ lua/c/plugins/fugitive.lua
@@ -3,5 +3,7 @@ return{
 	"tpope/vim-fugitive",
 	config = function ()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)		
+		vim.keymap.set("n","<leader>ga", ":Git add -A <CR>")
+		vim.keymap.set("n","<leader>gc", ":Git commit -m <CR>")
 	end
 }
